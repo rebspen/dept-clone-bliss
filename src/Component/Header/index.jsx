31c40ca5bbab9018ec475b838a599858b6e40fb1
Rebspen <rebeccaspence111@gmail.com>
2020-03-07 14:37:54 +0000

    first two sections resonsive

--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -5,6 +5,66 @@ import "./style.css";
 
 function Header(props) {
   return (
+    <div>
+    {/* header on mobile */}
+    <div class="headermobile" >
+    <nav className="navbar bg-none navbar-light">
+        <a className="navbar-brand" href="#" style ={{fontSize: "40px"}}>
+          DEPT
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsingNavbar"
+        >
+        <p className="navbar-brand menu">menu</p>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse z" id="collapsingNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">Home</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="//codeply.com">
+                xxxxx
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#myAlert" data-toggle="collapse">
+                xxxxx
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href=""
+                data-target="#myModal"
+                data-toggle="modal"
+              >
+                xxxxx
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="d-flex flex-column justify-content-between align-items-center">
+      <div className="content">
+        <h1 className="title">WORK</h1>
+      </div>
+      <button type="button" className="hdmobile">
+          <Link to ="#">VIEW CASE</Link>
+        </button>
+      </div>
+    </div>
+
+
+{/* header on desktop */}
     <div class="header pt-1">
       <nav className="navbar bg-none navbar-light">
         <a className="navbar-brand" href="#" style ={{fontSize: "40px"}}>
@@ -54,9 +114,10 @@ function Header(props) {
       <div className="content">
         <h1 className="title">WORK</h1>
         <button type="button" className="hd">
-          VIEW CASE
+        <Link to ="#"><p className="linkbtn">VIEW CASE</p></Link>
         </button>
       </div>
+    </div>
     </div>
   );
 }
--- src/Component/Searchbar/index.jsx
+++ src/Component/Searchbar/index.jsx
@@ -3,7 +3,31 @@ import "./style.css"
 
 function Searchbar(props) {
   return (
-    <div class="container">
+<div>
+<div class="container mobilesearchbar">
+      <div class="row m-3 mt-4 d-flex flex-column justify-content-start search">
+      <div class="d-flex flex-row">
+      <p>show me</p>
+        <form class="col-md-0">
+          <select>
+            <option>all work</option>
+            <option>videos</option>
+          </select>
+        </form>
+      </div>
+      <div class="d-flex flex-row">
+        <p>in </p>
+        <form class="col-md-0">
+          <select>
+            <option>all industries</option>
+            <option>fashion</option>
+          </select>
+        </form>
+      </div>
+      </div>
+    </div>
+
+    <div class="container searchbar">
       <div class="row m-3 mt-4 d-flex flex-row justify-content-end search">
       <p>show me ..</p>
         <form class="col-md-2">
@@ -21,6 +45,7 @@ function Searchbar(props) {
         </form>
       </div>
     </div>
+</div>
   );
 }
 
