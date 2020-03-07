6becd10f86aac6334919cebc4c70b673ba990106
Rebspen <rebeccaspence111@gmail.com>
2020-03-07 23:09:33 +0000

    json added with cases

--- src/Component/Case/index.jsx
+++ src/Component/Case/index.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css"
 
-function Case() {
+function Case(props) {
+  console.log("props", props)
   return (
     <div className="topcase">
-
     {/* on mobile */}
     <div className="casemobile">
     <Link className="link" to = "/"> 
-    <div className="card" style={{width: "100%", border:"none", borderRadius: "0px"}}>
-      <img src="/images/tui.png" className="card-img-top" alt="..."  style ={{width: "100%"}}/>
+    <div className="card" style={{maxWidth: "100%", border:"none", borderRadius: "0px"}}>
+      <img src={props.val.image} className="card-img-top" alt="..."  style ={{width: "100%"}}/>
       <div className="card-body">
-        <h5 className="card-title">Arla</h5>
+        <h5 className="card-title">{props.val.client}</h5>
         <p className="card-text">
-          Swipe to find your next holiday destination
+        {props.val.title}
         </p>
       </div>
     </div>
@@ -23,12 +23,12 @@ function Case() {
 
 {/* on desktop */}
     <div className="casedesktop">
-    <div className="card" style={{width: "100%", border:"none", borderRadius: "0px"}}>
-      <img src="/images/tui.png" className="card-img-top" alt="..." />
+    <div className="card" style={{maxWidth: "100%", border:"none", borderRadius: "0px"}}>
+      <img src={props.val.image} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Arla</h5>
+        <h5 className="card-title">{props.val.client}</h5>
         <p className="card-text">
-          Swipe to find your next holiday destination
+        {props.val.title}
         </p>
         <Link className="link" to = "/"> &#9658; VIEW CASE</Link>
       </div>
--- src/Views/Work.jsx
+++ src/Views/Work.jsx
@@ -9,28 +9,30 @@ import Clients from "../Component/Clients";
 import Contact from "../Component/Contact";
 import Footer from "../Component/Footer"
 import "./views.css";
+import cases from "./cases"
 
 function Work() {
+  console.log("here", cases)
   return (
     <div>
       <Header />
       <Searchbar />
       <div className="mainContent">
-        <Case />
-        <Case />
-        <Case />
-        <Case />
-        <CaseLargeLeft />
-        <Case />
-        <Case />
-        <CaseLargeRight />
-        <Case />
-        <Case />
-        <Case />
-        <Case />
+        <Case val= {cases[0]}/>
+        <Case val= {cases[1]}/>
+        <Case val= {cases[2]}/>
+        <Case val= {cases[3]}/>
+        <CaseLargeLeft val= {cases[4]} />
+        <Case val= {cases[5]} />
+        <Case val= {cases[6]} />
+        <CaseLargeRight val= {cases[7]} />
+        <Case val= {cases[8]} />
+        <Case val= {cases[9]} />
+        <Case val= {cases[10]} />
+        <Case val= {cases[11]} />
         <Quote />
-        <Case />
-        <Case />
+        <Case val= {cases[12]} />
+        <Case val= {cases[13]} />
       </div>
       <Clients />
       <Contact/>
